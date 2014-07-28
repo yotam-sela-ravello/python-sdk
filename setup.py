d8c6578b56fd3347bf60ae5d79ec8d9db715544c
Geert Jansen <geertj@gmail.com>
2014-07-28 14:09:49 +0200

    setup: add 'docopt' dependency

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
     setup(
         package_dir = { '': 'lib' },
         py_modules = ['ravello_sdk'],
-        install_requires = ['six'],
+        install_requires = ['six', 'docopt'],
         scripts = ['tools/ravello-create-nodes', 'tools/ravello-set-svm',
                    'tools/ravello-set-uuid'],
         **version_info
