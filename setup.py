bd084254859699fa7ff5506ec90c1157dba73edb
Geert Jansen <geertj@gmail.com>
2014-07-28 18:34:17 +0200

    start working on 1.11

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup
 
 version_info = {
     'name': 'ravello-sdk',
-    'version': '1.10',
+    'version': '1.11.dev',
     'description': 'Python SDK for the Ravello API',
     'author': 'Geert Jansen',
     'author_email': 'geert.jansen@ravellosystems.com',
