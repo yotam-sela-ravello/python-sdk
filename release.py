35ea1b2f58baabca204c0affed247b3bb706d871
johnyzed <zenoujohn@gmail.com>
2015-05-10 12:56:34 +0300

    adding non-interactive mode

--- release.py
+++ release.py
@@ -16,7 +16,12 @@ from __future__ import absolute_import, print_function
 
 import sys
 import subprocess
+import argparse
+import logging
 from setup import version_info as vinfo
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 if sys.version_info[0] == 2:
     input = raw_input
@@ -29,7 +34,7 @@ def sh(cmd, *args):
     ret = subprocess.call(cmd, shell=True)
     if ret == 0:
         return
-    print('Error: cmd {0!r} exited with status {1}'.format(cmd, ret), file=sys.stderr)
+    logger.error('Error: cmd {0!r} exited with status {1}'.format(cmd, ret), file=sys.stderr)
     sys.exit(1)
 
 
@@ -41,22 +46,23 @@ def confirm(prompt):
         response = input(prompt)
         if response in 'yn':
             break
-        print('Please answer "y" or "n"')
+        logger.info('Please answer "y" or "n"')
     return response == 'y'
 
 
-def get_release_versions():
+def get_release_versions(script_bool):
     """Prompt the user for the current and next release versions."""
     version = vinfo['version']
     if version.endswith('.dev'):
-        print('Current development version: {0}'.format(version))
+        logger.info('Current development version: {0}'.format(version))
         relver = version[:-4]
-        override = input('What version do you want to release [{0}]: '.format(relver))
-        if override:
-            relver = override
+        if not script_bool:
+            override = input('What version do you want to release [{0}]: '.format(relver))
+            if override:
+                relver = override
     else:
-        print('Current version is NOT a development version: {0}'.format(version))
-        if not confirm('Do you want to release this version?'):
+        logger.info('Current version is NOT a development version: {0}'.format(version))
+        if  not script_bool and not confirm('Do you want to release this version?'):
             return None, None
         relver = version
     try:
@@ -65,32 +71,36 @@ def get_release_versions():
         nextver = '.'.join(map(str, nums))
     except ValueError:
         nextver = ''
-    override = input('What will be the next version [{0}]: '.format(nextver))
-    if override:
-        nextver = override
-    print('Version to release: {0}'.format(relver))
-    print('New version to start: {0}'.format(nextver or '(none)'))
-    if not confirm('Is this correct?'):
+    if not script_bool:
+        override = input('What will be the next version [{0}]: '.format(nextver))
+        if override:
+            nextver = override
+    logger.info('Version to release: {0}'.format(relver))
+    logger.info('New version to start: {0}'.format(nextver or '(none)'))
+    if not script_bool and not confirm('Is this correct?'):
         return None, None
     return relver, nextver
 
 
-def confirm_file_list():
+def confirm_file_list(script_bool):
     """Confirm differences between files in Git and in the sdist."""
     sh('git ls-files | sort > files.git')
-    print('Comparing files in git against the source distribution...')
+    logger.info('Comparing files in git against the source distribution...')
     sh('python setup.py sdist >/dev/null 2>&1')
     sh('tar tvfz dist/ravello-sdk-{0}.tar.gz'
        ' | sed -e \'s/^.*{0}\///\' -e \'/\/$/d\' -e \'/^$/d\''
        ' | sort > files.sdist', vinfo['version'])
     sh('diff -u files.git files.sdist || true')
     sh('rm files.git; rm files.sdist')
-    return confirm('Are these changes OK?')
+    if not script_bool:
+        return confirm('Are these changes OK?')
+    else:
+        return True
 
 
 def make_release(relver, nextver):
     """Make a release for *relver*. Next version will be *nextver*."""
-    print('Creating the release..')
+    logger.info('Creating the release..')
     if relver != vinfo['version']:
         sh('sed -i -e \'s/{0}/{1}/\' setup.py', vinfo['version'], relver)
         sh('git add setup.py')
@@ -103,17 +113,35 @@ def make_release(relver, nextver):
         sh('git commit -m "start working on {0}"', nextver)
     sh('git push')
     sh('git push --tags')
-    print('Done!')
+    logger.info('Done!')
 
 
 def main():
-    relver, nextver = get_release_versions()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', action='store_true', default=False,
+                    dest='script_mode',
+                    help='Run the release in script mode , non-interactive')
+    parser.add_argument('-c', action='store', dest='current_version',
+                     help='Current version')
+    parser.add_argument('-n', action='store', dest='next_version',
+                     help='Next version')
+    results = parser.parse_args()
+    logger.info('script_mode  = %s' % results.script_mode)
+    logger.info('current_version   = %s' % results.current_version)
+    logger.info('next_version      = %s' % results.next_version)
+    if results.current_version is not None or results.next_version is not None:
+        if results.current_version is not None:
+            relver=results.current_version
+        if results.next_version is not None:
+            nextver=results.next_version
+        logger.info('Version to release: {0}'.format(relver))
+        logger.info('New version to start: {0}'.format(nextver or '(none)'))
+    else:
+        relver, nextver = get_release_versions(results.script_mode)
     if relver is None:
         return
-    if not confirm_file_list():
+    if not confirm_file_list(results.script_mode):
         return
     make_release(relver, nextver)
 
-
-if __name__ == '__main__':
-    main()
+main()
