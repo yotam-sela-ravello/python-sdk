755b09d5ec9702f62de46e465b10d906c3224ae9
Jonathan Zenou <johnyzed@users.noreply.github.com>
2016-10-18 11:57:02 +0300

    changed version to 2.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup
 
 version_info = {
     'name': 'ravello-sdk',
-    'version': '2.0',
+    'version': '2.1',
     'description': 'Python SDK for the Ravello API',
     'author': 'Geert Jansen',
     'maintainer': 'Hadar Davidovich',
