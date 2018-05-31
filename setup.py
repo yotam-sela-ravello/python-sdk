69204d9e72cc766faa5dfb3bafa670f9d2afa3a6
Jonathann Zenou <johnyzed@users.noreply.github.com>
2018-05-31 10:47:12 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup
 
 version_info = {
     'name': 'ravello-sdk',
-    'version': '2.13',
+    'version': '2.14',
     'description': 'Python SDK for the Ravello API',
     'author': 'Geert Jansen',
     'author_email': 'geert.jansen@ravellosystems.com',
